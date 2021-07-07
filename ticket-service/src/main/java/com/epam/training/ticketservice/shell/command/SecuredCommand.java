594ead4d8be8ec1628f7294d312304d413235af7
rkeeves <aprettydecentaddress@gmail.com>
2021-07-07 20:14:32 +0200

    feat: added SecuredCommand for availability checks

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/shell/command/SecuredCommand.java
@@ -0,0 +1,38 @@
+package com.epam.training.ticketservice.shell.command;
+
+import com.epam.training.ticketservice.core.security.SecurityService;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.Availability;
+
+@RequiredArgsConstructor
+public class SecuredCommand {
+
+    @Getter(AccessLevel.PROTECTED)
+    private final SecurityService securityService;
+
+    public Availability isAuthenticated() {
+        if (securityService.isAuthenticated()) {
+            return Availability.available();
+        } else {
+            return Availability.unavailable("You must be signed in to use this command");
+        }
+    }
+
+    public Availability isNotAuthenticated() {
+        if (!securityService.isAuthenticated()) {
+            return Availability.available();
+        } else {
+            return Availability.unavailable("You must NOT be signed in to use this command");
+        }
+    }
+
+    public Availability isPrivileged() {
+        if (securityService.isAuthenticated() && securityService.isPrivileged()) {
+            return Availability.available();
+        } else {
+            return Availability.unavailable("You must have privileges to use this command");
+        }
+    }
+}
