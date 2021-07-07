7ca0378c18c1e7a89979cd40accdacf7f0d5446b
rkeeves <aprettydecentaddress@gmail.com>
2021-07-07 22:14:58 +0200

    fix: added missing Configuration annotation

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/security/config/SecurityConfig.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.epam.training.ticketservice.core.security.config;
 
 import com.epam.training.ticketservice.core.security.service.AccountDetailsService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
