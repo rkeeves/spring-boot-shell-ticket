82512f275501e333d7979d8677471029fee2b1d2
rkeeves <aprettydecentaddress@gmail.com>
2021-07-07 22:03:25 +0200

    feat: wired in AccountRepository

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/security/service/AccountDetailsService.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/security/service/AccountDetailsService.java
@@ -1,5 +1,7 @@
 package com.epam.training.ticketservice.core.security.service;
 
+import com.epam.training.ticketservice.core.account.repository.AccountRepository;
+import com.epam.training.ticketservice.core.security.adapter.AccountToUserDetailsAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,8 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AccountDetailsService implements UserDetailsService {
 
+    private final AccountRepository accountRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        throw new UsernameNotFoundException(username);
+        return accountRepository.findByUsername(username)
+                .map(AccountToUserDetailsAdapter::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
     }
 }
