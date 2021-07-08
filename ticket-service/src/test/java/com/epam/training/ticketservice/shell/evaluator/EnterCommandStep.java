10c30a4650240b4894061d793c5b4f2965a46a4b
rkeeves <aprettydecentaddress@gmail.com>
2021-07-08 18:28:52 +0200

    feat: fix for shell quoted strings

--- ticket-service/src/test/java/com/epam/training/ticketservice/shell/evaluator/EnterCommandStep.java
+++ ticket-service/src/test/java/com/epam/training/ticketservice/shell/evaluator/EnterCommandStep.java
@@ -3,4 +3,6 @@ package com.epam.training.ticketservice.shell.evaluator;
 public interface EnterCommandStep {
 
     ExpectOutputStep afterCommand(String command);
+
+    ExpectOutputStep afterCommandByWords(String... words);
 }
--- ticket-service/src/test/java/com/epam/training/ticketservice/shell/evaluator/ShellEvaluator.java
+++ ticket-service/src/test/java/com/epam/training/ticketservice/shell/evaluator/ShellEvaluator.java
@@ -1,16 +1,20 @@
 package com.epam.training.ticketservice.shell.evaluator;
 
+import org.springframework.shell.Input;
 import org.springframework.shell.Shell;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ShellEvaluator implements EnterCommandStep, ExpectOutputStep {
 
     private Shell shell;
 
-    private String command;
+    private Input input;
 
     private ShellEvaluator(Shell shell) {
         this.shell = shell;
@@ -22,17 +26,38 @@ public class ShellEvaluator implements EnterCommandStep, ExpectOutputStep {
 
     @Override
     public ExpectOutputStep afterCommand(String command) {
-        this.command = command;
+        this.input = () -> command;
+        return this;
+    }
+
+    @Override
+    public ExpectOutputStep afterCommandByWords(String... words) {
+        this.input = inputOfWords(words);
         return this;
     }
 
     @Override
     public void expectOutput(String... lines) {
-        assertEquals(Arrays.asList(lines), shell.evaluate(() -> command));
+        assertEquals(Arrays.asList(lines), shell.evaluate(input));
     }
 
     public static void commandIsNotAvailable(Shell shell, String command) {
         var method = shell.listCommands().get(command);
         assertFalse(method.getAvailability().isAvailable());
+    }
+
+    public static Input inputOfWords(String... words) {
+        return new Input() {
+
+            @Override
+            public String rawText() {
+                return Arrays.stream(words).collect(Collectors.joining(" "));
+            }
+
+            @Override
+            public List<String> words() {
+                return List.of(words);
+            }
+        };
     }
 }
