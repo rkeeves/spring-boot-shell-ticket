9c06a5c83c4e876279fd945cfba305819de4e52b
rkeeves <aprettydecentaddress@gmail.com>
2021-07-09 12:48:23 +0200

    refactor: removed optional string handler bean

--- ticket-service/src/main/java/com/epam/training/ticketservice/shell/config/ShellConfiguration.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/shell/config/ShellConfiguration.java
@@ -1,16 +1,10 @@
 package com.epam.training.ticketservice.shell.config;
 
-import com.epam.training.ticketservice.shell.resulthandler.OptionalStringResultHandler;
-import org.jline.terminal.Terminal;
 import org.jline.utils.AttributedString;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.shell.ResultHandler;
 import org.springframework.shell.jline.PromptProvider;
-
-import java.util.Optional;
 
 @Configuration
 public class ShellConfiguration {
@@ -21,10 +15,5 @@ public class ShellConfiguration {
     @Bean
     public PromptProvider tickerServicePrompt() {
         return () -> new AttributedString(prompt);
-    }
-
-    @Bean
-    public ResultHandler<Optional<String>> shellHelper(@Lazy Terminal terminal) {
-        return new OptionalStringResultHandler(terminal);
     }
 }
