5bb3b58727de8a968fb4fc93214601f2ae0b69b9
rkeeves <aprettydecentaddress@gmail.com>
2021-07-09 16:34:08 +0200

    refactor: deleted no longer used interfaces

--- ticket-service/src/test/java/com/epam/training/ticketservice/shell/evaluator/EnterCommandStep.java
+++
@@ -1,8 +0,0 @@
-package com.epam.training.ticketservice.shell.evaluator;
-
-public interface EnterCommandStep {
-
-    ExpectOutputStep afterCommand(String command);
-
-    ExpectOutputStep afterCommandByWords(String... words);
-}
--- ticket-service/src/test/java/com/epam/training/ticketservice/shell/evaluator/ExpectOutputStep.java
+++
@@ -1,6 +0,0 @@
-package com.epam.training.ticketservice.shell.evaluator;
-
-public interface ExpectOutputStep {
-
-    void expectOutput(String... lines);
-}
