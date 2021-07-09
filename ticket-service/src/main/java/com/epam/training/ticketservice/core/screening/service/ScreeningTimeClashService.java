e82fba4ce4ff6bae18a363363bfda0cb11079d5c
rkeeves <aprettydecentaddress@gmail.com>
2021-07-09 12:23:15 +0200

    refactor: decreased responsibilities

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/service/ScreeningTimeClashService.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/service/ScreeningTimeClashService.java
@@ -1,11 +1,13 @@
 package com.epam.training.ticketservice.core.screening.service;
 
-import com.epam.training.ticketservice.core.room.entity.Room;
 import com.epam.training.ticketservice.core.screening.exception.ScreeningTimeClashException;
 
 import java.time.LocalDateTime;
 
 public interface ScreeningTimeClashService {
 
-    void checkNoClash(int duration, Room room, LocalDateTime startDateTime) throws ScreeningTimeClashException;
+    void check(LocalDateTime start,
+          LocalDateTime end,
+          LocalDateTime existingStart,
+          int existingDuration) throws ScreeningTimeClashException;
 }
