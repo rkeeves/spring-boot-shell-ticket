551ff6218ce79a503a1d2d86f5dc7e27226da9f3
rkeeves <aprettydecentaddress@gmail.com>
2021-07-12 20:40:20 +0200

    fix: rewritten parametrized test for readability

--- ticket-service/src/test/java/com/epam/training/ticketservice/core/booking/dto/SeatTest.java
+++ ticket-service/src/test/java/com/epam/training/ticketservice/core/booking/dto/SeatTest.java
@@ -4,30 +4,35 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SeatTest {
 
     @ParameterizedTest
     @MethodSource("provideForCompareTo")
-    void test_compareTo(Seat first, Seat second, int expected) {
+    void test_compareTo(Seat first, Seat second) {
+        var expected = Comparator.comparingInt(Seat::getRow)
+                .thenComparing(Seat::getColumn)
+                .compare(first, second);
         assertEquals(expected, first.compareTo(second));
     }
 
     static Stream<Arguments> provideForCompareTo() {
-        return Stream.of(
-                Arguments.of(Seat.of(1, 1), Seat.of(2, 2), -1),
-                Arguments.of(Seat.of(1, 2), Seat.of(2, 2), -1),
-                Arguments.of(Seat.of(1, 3), Seat.of(2, 2), -1),
-                Arguments.of(Seat.of(1, 1), Seat.of(1, 2), -1),
-                Arguments.of(Seat.of(2, 1), Seat.of(1, 2), 1),
-                Arguments.of(Seat.of(2, 2), Seat.of(1, 2), 1),
-                Arguments.of(Seat.of(2, 3), Seat.of(1, 2), 1),
-                Arguments.of(Seat.of(2, 3), Seat.of(2, 2), 1),
-                Arguments.of(Seat.of(2, 2), Seat.of(1, 2), 1),
-                Arguments.of(Seat.of(2, 2), Seat.of(2, 2), 0)
-        );
+        var list = new ArrayList<Arguments>();
+        for (int rowA = 0; rowA < 3; rowA++) {
+            for (int colA = 0; colA < 3; colA++) {
+                var seatA = Seat.of(rowA, colA);
+                for (int rowB = 0; rowB < 3; rowB++) {
+                    for (int colB = 0; colB < 3; colB++) {
+                        list.add(Arguments.of(seatA, Seat.of(rowB, colB)));
+                    }
+                }
+            }
+        }
+        return list.stream();
     }
 }
