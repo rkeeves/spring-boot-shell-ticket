177530659dff958349129b34c4cfac43453f7c51
rkeeves <aprettydecentaddress@gmail.com>
2021-07-12 17:26:38 +0200

    feat: Screening oneToMany relation with Booking

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/entity/Screening.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/entity/Screening.java
@@ -1,5 +1,6 @@
 package com.epam.training.ticketservice.core.screening.entity;
 
+import com.epam.training.ticketservice.core.booking.entity.Booking;
 import com.epam.training.ticketservice.core.movie.entity.Movie;
 import com.epam.training.ticketservice.core.room.entity.Room;
 import lombok.Builder;
@@ -7,13 +8,17 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "screenings")
@@ -32,6 +37,11 @@ public class Screening {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @MapsId("roomId")
     private Room room;
+
+    @OneToMany(mappedBy = "screening",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private Set<Booking> bookings = new HashSet<>();
 
     @Builder
     public Screening(Movie movie, Room room, LocalDateTime startDateTime) {
