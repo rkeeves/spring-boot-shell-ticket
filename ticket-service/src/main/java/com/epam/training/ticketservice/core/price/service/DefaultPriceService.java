1a750c21d91e8124010fdfd944b605b18f30a722
rkeeves <aprettydecentaddress@gmail.com>
2021-07-12 22:21:01 +0200

    feat: new methods without implementation

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/price/service/DefaultPriceService.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/price/service/DefaultPriceService.java
@@ -4,11 +4,18 @@ import com.epam.training.ticketservice.core.screening.entity.Screening;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class DefaultPriceService implements PriceService {
 
     private int basePrice = 1500;
+
+    @Override
+    public int getPerSeatPriceBy(String movieTitle, String roomName, LocalDateTime startDateTime) {
+        return 0;
+    }
 
     @Override
     public int getPerSeatPriceBy(Screening screening) {
@@ -18,5 +25,25 @@ public class DefaultPriceService implements PriceService {
     @Override
     public void updateBasePrice(int newBasePrice) {
         basePrice = newBasePrice;
+    }
+
+    @Override
+    public void createPriceComponent(String name, int newBasePrice) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void attachPriceComponentToMovie(String priceComponent, String movieTitle) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void attachPriceComponentToRoom(String priceComponent, String roomName) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void attachPriceComponentToScreening(String priceComponent, String movieTitle, String roomName, LocalDateTime startDateTime) {
+        throw new RuntimeException();
     }
 }
