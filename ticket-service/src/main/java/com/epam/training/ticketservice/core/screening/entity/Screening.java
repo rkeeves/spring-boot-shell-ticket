9965d4c80267c1682243c186c29ee217d3f95c41
rkeeves <aprettydecentaddress@gmail.com>
2021-07-12 23:14:17 +0200

    feat: added PriceComponents

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/entity/Screening.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/entity/Screening.java
@@ -2,20 +2,15 @@ package com.epam.training.ticketservice.core.screening.entity;
 
 import com.epam.training.ticketservice.core.booking.entity.Booking;
 import com.epam.training.ticketservice.core.movie.entity.Movie;
+import com.epam.training.ticketservice.core.price.entity.PriceComponent;
+import com.epam.training.ticketservice.core.price.service.Priceable;
 import com.epam.training.ticketservice.core.room.entity.Room;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -25,7 +20,7 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @EqualsAndHashCode(exclude = {"movie", "room"})
-public class Screening {
+public class Screening implements Priceable {
 
     @EmbeddedId
     private ScreeningId id;
@@ -42,6 +37,20 @@ public class Screening {
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private Set<Booking> bookings = new HashSet<>();
+
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "screening_price",
+            joinColumns = {
+                    @JoinColumn(name = "screening_movie_id", referencedColumnName = "movie_id"),
+                    @JoinColumn(name = "screening_room_id", referencedColumnName = "room_id"),
+                    @JoinColumn(name = "screening_start_date_time", referencedColumnName = "start_date_time")
+            },
+            inverseJoinColumns = @JoinColumn(name = "price_id")
+    )
+    private Set<PriceComponent> priceComponents = new HashSet<>();
 
     @Builder
     public Screening(Movie movie, Room room, LocalDateTime startDateTime) {
