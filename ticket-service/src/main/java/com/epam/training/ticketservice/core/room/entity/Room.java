36ce92820405871190447cca6d7c8bcfde1d3a19
rkeeves <aprettydecentaddress@gmail.com>
2021-07-12 23:14:00 +0200

    feat: added PriceComponents

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/room/entity/Room.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/room/entity/Room.java
@@ -1,16 +1,13 @@
 package com.epam.training.ticketservice.core.room.entity;
 
+import com.epam.training.ticketservice.core.price.entity.PriceComponent;
+import com.epam.training.ticketservice.core.price.service.Priceable;
 import com.epam.training.ticketservice.core.screening.entity.Screening;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -20,7 +17,8 @@ import java.util.Set;
 @Entity
 @Table(name = "rooms")
 @Data
-public class Room {
+@NoArgsConstructor
+public class Room implements Priceable {
 
     @Id
     @GeneratedValue
@@ -46,6 +44,16 @@ public class Room {
             orphanRemoval = true)
     private Set<Screening> screenings = new HashSet<>();
 
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "room_price",
+            joinColumns = @JoinColumn(name = "room_id"),
+            inverseJoinColumns = @JoinColumn(name = "price_id")
+    )
+    private Set<PriceComponent> priceComponents = new HashSet<>();
+
     @Builder
     public Room(Long id,
                 @NotNull @NotEmpty String name,
@@ -55,9 +63,5 @@ public class Room {
         this.name = name;
         this.rows = rows;
         this.columns = columns;
-    }
-
-    public Room() {
-
     }
 }
