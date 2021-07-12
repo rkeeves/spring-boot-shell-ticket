6d9b17a2bf7db5123ccab50662c3e358e3ea47e4
rkeeves <aprettydecentaddress@gmail.com>
2021-07-12 18:51:51 +0200

    feat: added static factory method

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/dto/Seat.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/booking/dto/Seat.java
@@ -11,6 +11,13 @@ public class Seat implements Comparable<Seat> {
 
     private final int column;
 
+    public static Seat of(int row, int column) {
+        return Seat.builder()
+                .row(row)
+                .column(column)
+                .build();
+    }
+
     @Override
     public int compareTo(Seat o) {
         if (row < o.row || (row == o.row && column < o.column)) {
